0724695f0c0cc6dab2c36c2acc47a5c59395eec4
Evgeny Shishkin <johnkil78@gmail.com>
2015-10-27 17:43:29 +0300

    Support runtime permissions.
    See http://android-developers.blogspot.ru/2015/09/google-play-services-81-and-android-60.html

--- app/src/main/java/com/devspark/metro/MainActivity.java
+++ app/src/main/java/com/devspark/metro/MainActivity.java
@@ -1,12 +1,21 @@
 package com.devspark.metro;
 
-import android.app.Activity;
+import android.Manifest;
+import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresPermission;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.util.SparseArrayCompat;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.davemorrissey.labs.subscaleview.ImageViewState;
@@ -15,21 +24,32 @@ import com.devspark.metro.util.Locations;
 import com.devspark.metro.util.SparseArrays;
 import com.devspark.metro.widget.MetroMapView;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends Activity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import static com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
+
+public class MainActivity extends AppCompatActivity
+        implements ConnectionCallbacks, OnConnectionFailedListener {
+    private static final int REQUEST_CODE_GOOGLE_API = 1;
+    private static final int REQUEST_CODE_LOCATION = 2;
+
+    private static final String STATE_RESOLVING_ERROR = "resolving_error";
     private static final String STATE_MAP_VIEW = "map_view";
     private static final String STATE_CITY = "city";
 
     private MetroMapView mMetroMapView;
     private IntPreference mCityIdPref;
-    private GoogleApiClient mGoogleApiClient;
-
     private SparseArrayCompat<City> mCities;
+
     private City mCurrentCity;
+
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mResolvingError = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +57,8 @@ public class MainActivity extends Activity implements
         setContentView(R.layout.activity_main);
 
         mMetroMapView = (MetroMapView) findViewById(R.id.map);
-        FloatingActionButton settingsButton = (FloatingActionButton) findViewById(R.id.fab_settings);
+        FloatingActionButton settingsButton =
+                (FloatingActionButton) findViewById(R.id.fab_settings);
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,19 +68,24 @@ public class MainActivity extends Activity implements
 
         mCityIdPref = new IntPreference(getPreferences(MODE_PRIVATE), "city_id");
         initCities();
+
         buildGoogleApiClient();
 
-        if (savedInstanceState != null && savedInstanceState.containsKey(STATE_CITY)) {
-            mCurrentCity = savedInstanceState.getParcelable(STATE_CITY);
-            ImageViewState mapState = (ImageViewState) savedInstanceState.getSerializable(STATE_MAP_VIEW);
-            mMetroMapView.showMap(mCurrentCity, mapState);
+        if (savedInstanceState != null) {
+            mResolvingError = savedInstanceState.getBoolean(STATE_RESOLVING_ERROR, false);
+            if (savedInstanceState.containsKey(STATE_CITY)) {
+                mCurrentCity = savedInstanceState.getParcelable(STATE_CITY);
+                ImageViewState mapState =
+                        (ImageViewState) savedInstanceState.getSerializable(STATE_MAP_VIEW);
+                mMetroMapView.showMap(mCurrentCity, mapState);
+            }
         }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (mCurrentCity == null) mGoogleApiClient.connect();
+        if (mCurrentCity == null && !mResolvingError) mGoogleApiClient.connect();
     }
 
     @Override
@@ -70,6 +96,7 @@ public class MainActivity extends Activity implements
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(STATE_RESOLVING_ERROR, mResolvingError);
         if (mCurrentCity != null) {
             outState.putParcelable(STATE_CITY, mCurrentCity);
             outState.putSerializable(STATE_MAP_VIEW, mMetroMapView.getState());
@@ -127,23 +154,117 @@ public class MainActivity extends Activity implements
         return mCities.get(mCityIdPref.get());
     }
 
-    @Override
-    public void onConnected(Bundle bundle) {
+    @RequiresPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+    private void detectCurrentCity() {
         Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-
         City currentCity = getDefaultCity();
         if (lastLocation != null) {
-            currentCity = Locations.nearestCity(lastLocation, SparseArrays.asList(mCities), currentCity);
+            currentCity = Locations.nearestCity(
+                    lastLocation, SparseArrays.asList(mCities), currentCity);
         }
         selectCity(currentCity);
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        selectCity(getDefaultCity());
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_GOOGLE_API) {
+            mResolvingError = false;
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_LOCATION) {
+            if (grantResults.length == 1 && grantResults[0] == PERMISSION_GRANTED) {
+                //noinspection ResourceType
+                detectCurrentCity();
+            } else {
+                // Permission was denied or request was cancelled
+                selectCity(getDefaultCity());
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Request missing location permission.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_CODE_LOCATION);
+        } else {
+            // Location permission has been granted.
+            detectCurrentCity();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            return;
+        } else if (result.hasResolution()) {
+            try {
+                mResolvingError = true;
+                result.startResolutionForResult(this, REQUEST_CODE_GOOGLE_API);
+            } catch (IntentSender.SendIntentException e) {
+                mGoogleApiClient.connect();
+            }
+        } else {
+            showErrorDialog(result.getErrorCode());
+            mResolvingError = true;
+        }
+        selectCity(getDefaultCity());
+    }
+
+    /* Show a dialog using GoogleApiAvailability.getErrorDialog() for an error message */
+    private void showErrorDialog(int errorCode) {
+        GoogleApiErrorDialog dialogFragment = GoogleApiErrorDialog.newInstance(errorCode);
+        dialogFragment.show(getSupportFragmentManager(), "error_dialog");
+    }
+
+    /* Called from GoogleApiErrorDialog when the dialog is dismissed. */
+    private void onDialogDismissed() {
+        mResolvingError = false;
+    }
+
+    public static class GoogleApiErrorDialog extends DialogFragment {
+        private static final String ARG_ERROR_CODE = "error_code";
+
+        public static GoogleApiErrorDialog newInstance(int errorCode) {
+            GoogleApiErrorDialog dialogFragment = new GoogleApiErrorDialog();
+            Bundle args = new Bundle();
+            args.putInt(ARG_ERROR_CODE, errorCode);
+            dialogFragment.setArguments(args);
+            return dialogFragment;
+        }
+
+        public GoogleApiErrorDialog() {}
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Get the error code and retrieve the appropriate dialog
+            int errorCode = getArguments().getInt(ARG_ERROR_CODE);
+            return GoogleApiAvailability.getInstance().getErrorDialog(
+                    getActivity(), errorCode, REQUEST_CODE_GOOGLE_API);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            ((MainActivity) getActivity()).onDialogDismissed();
+        }
     }
 }
